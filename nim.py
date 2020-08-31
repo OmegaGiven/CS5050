7c39ff4b85a5724525de035025258cd3a17ca171
OmegaGiven <nathan9j@gmail.com>
2020-08-31 09:20:58 -0600

    adding nim game algorithm

---
+++ nim.py
@@ -0,0 +1,19 @@
+"""
+Rules:
+2 player game
+Action: You can take one or two stones
+if its your turn and there is 1 stone left you win
+else you lose.
+"""
+
+
+def win(n):
+    """
+        n = number of stones
+    """
+    if n == 0:
+        return False
+    if n == 1:
+        return True
+
+    return win(n-1)
