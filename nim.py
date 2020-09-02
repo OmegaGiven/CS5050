16c0ba82653394d99136f9dee75521fa647a626d
OmegaGiven <nathan9j@gmail.com>
2020-09-02 08:58:30 -0600

    creating the nim game

--- nim.py
+++ nim.py
@@ -6,6 +6,13 @@ if its your turn and there is 1 stone left you win
 else you lose.
 """
 
+total_count = 0
+
+
+def ai(n, total_count):
+     if win(n):
+         total_count = total_count - 1
+
 
 def win(n):
     """
@@ -16,4 +23,17 @@ def win(n):
     if n == 1:
         return True
 
-    return not (win(n-1) and win(n-2))
+    # return not (win(n-1) and win(n-2))
+
+
+if __name__ == '__main__':
+    total_count = input("How many stones: ")
+    win = False
+    while not win:
+        how_many = int(input("pick 1 or 2 stones(type 1 or 2): "))
+        total_count - how_many
+        if win(total_count):
+            print("You have Won")
+        ai(total_count, total_count)
+        if win(total_count):
+            print("AI has Won")
